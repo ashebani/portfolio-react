54727317d1db1a0277b76994ad95db88697ec3cb
ashebani <azizoo2001.as@gmail.com>
2023-06-10 17:09:32 +0200

    added data.js file

---
+++ src/utils/data.js
@@ -0,0 +1,101 @@
+// Done
+
+export const hero = {
+  title: `Hello! I'm Abdulaziz, a Front-end Developer`,
+  description: `Welcome to my website! This website was created as a showcase of my portfolio in web development. As a passionate developer, I have always been fascinated by the endless possibilities that technology provides us.`,
+};
+
+export const services = {
+  title: "The Service I offer is specifically designed to meet your needs.",
+  description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+  doloribus adipisci. Nisi facere veritatis explicabo at, sunt fuga
+  voluptate quod iure, ipsam ipsa amet. Ratione, non earum accusantium
+  inventore ab totam impedit suscipit nulla natus alias? Voluptates
+  doloribus qui harum!`,
+  servicesList: [
+    {
+      name: "User Research",
+      image: require("../assets/design-3.svg").default,
+      describtion: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+          doloribus adipisci. Nisi facere veritatis explicabo at, sunt fuga
+          voluptate quod iure, ipsam ipsa amet.`,
+    },
+    {
+      name: "Brand Design",
+      image: require("../assets/design-2.svg").default,
+      describtion: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+          doloribus adipisci. Nisi facere veritatis explicabo at, sunt fuga
+          voluptate quod iure, ipsam ipsa amet.`,
+    },
+    {
+      name: "App Design",
+      image: require("../assets/design-1.svg").default,
+      describtion: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+          doloribus adipisci. Nisi facere veritatis explicabo at, sunt fuga
+          voluptate quod iure, ipsam ipsa amet.`,
+    },
+    {
+      name: "Web Design",
+      image: require("../assets/design-4.svg").default,
+      describtion: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
+          doloribus adipisci. Nisi facere veritatis explicabo at, sunt fuga
+          voluptate quod iure, ipsam ipsa amet.`,
+    },
+  ],
+};
+
+// Done
+export const aboutMe = {
+  title: "More About Me",
+  description: `I am passionate about learning new things and staying up-to-date with the latest technologies and trends in my field.`,
+  moreInfo: [
+    `In my free time, I am always looking for ways to improve my skills and expand my knowledge, and I believe that continuous learning is essential to staying ahead in today's fast-paced world.`,
+    `I also believe that working in a team is essential to achieving success, and I enjoy collaborating with others to solve complex problems and deliver high-quality results. I am a strong communicator and believe that clear and concise communication is key to building strong relationships with clients and colleagues.`,
+  ],
+};
+
+// Done
+export const experience = {
+  title: "My experience",
+  description: `While I may be new to the field, I am passionate about learning and have already gained experience in working with React, I am excited to continue growing my skills and knowledge in this field and am eager to take on new challenges and opportunities.`,
+  workHistory: [
+    {
+      time: "Jan 2023 - Present",
+      position: `Co-Founder | Marvel Tech`,
+      jobDescription: `I Started my company with my Partner with the main focus on providing IT solutions to different libyan entities, and push towards digital transformation in Libya.`,
+    },
+
+    {
+      time: "Nov 2022 - Feb 2023",
+      position: "Content Writer | GDSC",
+      jobDescription: `Google Developers Students Club, made for students in the university, specialized in giving lectures and courses across the university, currently working with their team as a content creator.`,
+    },
+    {
+      time: "Feb 2022 - Present",
+      position: "Data Analyst | Mirrors LLC",
+      jobDescription: `As a data analyst, my role involves collecting, analyzing, and interpreting large sets of data to help customers make informed business decisions. This would include identifying trends, patterns, and insights that can be used to improve operations, increase efficiency, and drive growth.`,
+    },
+    {
+      time: "Aug 2021 - Present",
+      position: "Social Media Manager | Mirrors LLC",
+      jobDescription: `As a social media manager, my role would involve managing the social media presence of the various companies or brands. This would include creating and curating content, scheduling posts, engaging with followers, and analyzing metrics to measure the success of the social media campaigns.`,
+    },
+  ],
+};
+
+// Done
+export const resume = {
+  title: `Got a project in mind?
+  Let's work it together!`,
+  description: `If you need help with web developement, I am here to provide exceptional service and help you achieve your business objectives.`,
+};
+
+// Done
+export const footer = {
+  socialMediaNames: [
+    { name: "Facebook", url: "https://www.facebook.com/abdulazizshebani" },
+    { name: "Instagram", url: "https://www.instagram.com/abdulaziz.shebani/" },
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/abdulazizshebani/" },
+    { name: "Github", url: "https://github.com/ashebani" },
+  ],
+};
